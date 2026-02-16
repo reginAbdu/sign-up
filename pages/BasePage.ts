03431503554a9240b0f1d88105a8173719a29c64
reginAbdu <reginaabdurakhmanova0101@gmail.com>
2026-02-16 15:45:34 +0500

    removed

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -11,27 +11,7 @@ export class BasePage {
         return await this.page.url();
     }
 
-    async getTitle() {
-        return await this.page.title();
-    }
-
     async waitForURL(urlPattern: string | RegExp) {
         await this.page.waitForURL(urlPattern);
-    }
-
-    async goBack() {
-        await this.page.goBack();
-    }
-
-    async reload() {
-        await this.page.reload();
-    }
-
-    async waitForSelector(selector: string) {
-        await this.page.waitForSelector(selector);
-    }
-
-    getHeader(headerText: string) {
-        return this.page.locator(`h4:has-text("${headerText}")`);
     }
 }
--- pages/PageFactory.ts
+++ pages/PageFactory.ts
@@ -1,18 +1,8 @@
 import { Page } from '@playwright/test';
-import { HomePage } from './HomePage';
-import { PropertyEstimatePage } from './PropertyEstimatePage';
 import { XometrySignUpPage } from './SignUpPage';
 
 export class PageFactory {
     constructor(private page: Page) { }
-
-    getHomePage(): HomePage {
-        return new HomePage(this.page);
-    }
-
-    getPropertyEstimatePage(): PropertyEstimatePage {
-        return new PropertyEstimatePage(this.page);
-    }
 
     getXometrySignUpPage(): XometrySignUpPage {
         return new XometrySignUpPage(this.page);
